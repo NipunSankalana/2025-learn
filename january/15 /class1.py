3886696108d07a518d33df964554ca70d5e65e61
Nipun <nipunsankalana98@gmail.com>
2026-01-17 08:27:47 +0530

    add class to another class initialization

--- january/15 /class1.py
+++ january/15 /class1.py
@@ -8,9 +8,27 @@ class coordinate(object):
         y_diff_sq= (self.y-other.y)**2
         distance = (x_diff_sq+y_diff_sq)**0.5
         return distance
+    
+    def to_origin(self):
+        self.x=0
+        self.y=0
+
+class circle(object):
+    def __init__(self,coordi,radius):
+        if type(coordi) != coordinate:
+            raise ValueError
+        if type(radius)!=int:
+            raise ValueError
+        self.cordi=coordinate
+        self.r=radius
+
 
 c=coordinate(3,4)
 origin=coordinate(0,0)
 
 print(c.distance(origin))
 print(coordinate.distance(c,origin))
+#c.to_origin()
+#print(c.x,c.y)
+
+c1=circle(c,5)
